bfe7ad4aba2321a99edb909b7a8410ca1e772d4f
Gabriel Claudino <gabrielhc@protonmail.com>
2021-11-10 22:11:43 -0300

    add exercise: basic - 2. lasagna

---
+++ basics/2. lasagna/lasagna.ex
@@ -0,0 +1,23 @@
+defmodule Lasagna do
+  @spec expected_minutes_in_oven :: 40
+  def expected_minutes_in_oven, do: 40
+
+  @spec remaining_minutes_in_oven(number) :: number
+  def remaining_minutes_in_oven(time) do
+    expected_minutes_in_oven() - time
+  end
+
+  @spec preparation_time_in_minutes(number) :: number
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
+
+  @spec total_time_in_minutes(number, number) :: number
+  def total_time_in_minutes(layers, time) do
+    preparation_time_in_minutes(layers) + time
+  end
+
+  def alarm do
+    "Ding!"
+  end
+end
