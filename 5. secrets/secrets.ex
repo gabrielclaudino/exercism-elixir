96c97b34152ca5330ed593b0ad1267f0ff76abf1
Gabriel Claudino <gabrielhc@protonmail.com>
2021-11-12 20:34:43 -0300

    feat(5. secrets): new exercise

---
+++ 5. secrets/secrets.ex
@@ -0,0 +1,19 @@
+use Bitwise
+
+defmodule Secrets do
+  def secret_add(secret), do: fn n -> n + secret end
+
+  def secret_subtract(secret), do: fn n -> n - secret end
+
+  def secret_multiply(secret), do: fn n -> n * secret end
+
+  def secret_divide(secret), do: fn n -> div(n, secret) end
+
+  def secret_and(secret), do: fn n -> n &&& secret end
+
+  def secret_xor(secret), do: fn n -> bxor(n, secret) end
+
+  def secret_combine(secret_function1, secret_function2) do
+    fn n -> n |> secret_function1.() |> secret_function2.() end
+  end
+end
